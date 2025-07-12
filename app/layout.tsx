90dcaee4871154d1a3cd51ac1c7d3d3636e0460e
unknown <rodolfor86@gmail.com>
2025-07-12 15:57:37 -0300

    Agregar favicon y corregir meta tag obsoleto - Solucionar errores 404 y deprecation warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,9 +57,15 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/favicon.png" />
+        
         {/* PWA Manifest */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#E65C37" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="MIMI Distribuidores" />
