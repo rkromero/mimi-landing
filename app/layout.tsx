e75c202e18cdd822917e275ef2fe3a57a5035ee6
unknown <rodolfor86@gmail.com>
2025-07-28 15:06:43 -0300

    Agregar Meta Pixel con ID 998834822204439

--- app/layout.tsx
+++ app/layout.tsx
@@ -116,23 +116,21 @@ export default function RootLayout({
           </>
         )}
         
-        {/* Meta Pixel (Facebook) */}
-        {META_PIXEL_ID && (
-          <Script id="meta-pixel" strategy="afterInteractive">
-            {`
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${META_PIXEL_ID}');
-              fbq('track', 'PageView');
-            `}
-          </Script>
-        )}
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '998834822204439');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         
         {/* Hotjar o similar (opcional) */}
         {process.env.NEXT_PUBLIC_HOTJAR_ID && (
@@ -153,17 +151,15 @@ export default function RootLayout({
       <body suppressHydrationWarning={true}>
         {children}
         
-        {/* NoScript para Meta Pixel */}
-        {META_PIXEL_ID && (
-          <noscript>
-            <img 
-              height="1" 
-              width="1" 
-              style={{display: 'none'}}
-              src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
-            />
-          </noscript>
-        )}
+        {/* End Meta Pixel Code */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1" 
+            style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=998834822204439&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   )
